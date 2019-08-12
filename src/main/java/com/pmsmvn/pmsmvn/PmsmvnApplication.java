d569534c29107c3a8a7c515d4d233eb7ee039d7a
[l220620411] <[2829596270@qq.com]>
2019-08-12 15:43:35 +0800

    提交

---
+++ src/main/java/com/pmsmvn/pmsmvn/PmsmvnApplication.java
@@ -0,0 +1,13 @@
+package com.pmsmvn.pmsmvn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PmsmvnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PmsmvnApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pmsmvn/pmsmvn/PmsmvnApplicationTests.java
@@ -0,0 +1,16 @@
+package com.pmsmvn.pmsmvn;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PmsmvnApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
